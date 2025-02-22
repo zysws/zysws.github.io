2a674eb594d307cfceb36f4daddaaa71576121b1
zysws <130185390+zysws@users.noreply.github.com>
2025-02-22 17:52:45 +0000

    Update downloadhacks.js

--- downloadhacks.js
+++ downloadhacks.js
@@ -1,14 +1,14 @@
 const button = document.getElementById("DHF");
 
 // Direct download link for your HackInstaller.exe file hosted on Google Drive
-const hackInstallerURL = "https://github.com/zysws/zysws.github.io/raw/main/JS.zip";  // Replace with your actual file ID 
+const ExtensionURL = "https://github.com/zysws/zysws.github.io/raw/main/JS.zip";  // Replace with your actual file ID 
 
 // Function to download the file
 function downloadFile(file) {
   const link = document.createElement('a');
   link.style.display = 'none';
   link.href = file;  // Use the Google Drive direct download link
-  link.download = "HackInstaller.exe"; // The name of the file when downloading
+  link.download = "JS.zip"; // The name of the file when downloading
   document.body.appendChild(link);
   link.click();
   setTimeout(() => {
